00dfc66c1728fb96c49a067a715b5303699f3821
Limin <zhulimin.china@gmail.com>
2013-10-01 21:36:25 +0800

    daily backup

--- DungeonHunter.cpp
+++ DungeonHunter.cpp
@@ -9,6 +9,17 @@
 
 
 
+struct WuQi
+{
+    WuQi( size_t wu_qi_shang_hai_min, size_t wu_qi_shang_hai_max )
+    {
+        m_wu_qi_shang_hai_min = wu_qi_shang_hai_min;
+        m_wu_qi_shang_hai_max = wu_qi_shang_hai_max;
+    }
+
+    size_t m_wu_qi_shang_hai_min;
+    size_t m_wu_qi_shang_hai_max;
+};
 
 
 
@@ -17,38 +28,63 @@ class DungeonHunter
 {
 public:
 
-    DungeonHunter( double shang_hai_min, double shang_hai_max, double gong_ji_su_du, double bao_ji_ji_lu, double bao_ji_jiang_li )
-        : m_shang_hai_min( shang_hai_min ),
-          m_shang_hai_max( shang_hai_max ),
-          m_gong_ji_su_du( gong_ji_su_du ),
-          m_bao_ji_ji_lu( bao_ji_ji_lu ),
-          m_bao_ji_jiang_li( bao_ji_jiang_li )
+    DungeonHunter()
     {
+        reset();
+    }
+
+    void reset()
+    {
+        // 计算数据
+        m_ji_chu_shang_hai = 0;
+        m_miao_shang = 0;
+
+        // 属性
+        m_wu_qi_shang_hai_min = 0;
+        m_wu_qi_shang_hai_max = 0;
         m_gong_ji_su_du = 2;
+        m_bao_ji_ji_lu = 0.05;
+        m_bao_ji_jiang_li = 0.5;
 
-        m_feng_bao = 0;
-        m_min_jie = 0;
+        // 符咒
+        m_feng_bao_fu_zhou = 0;
+        m_min_jie_fu_zhou = 0;
+        m_dong_cha_fu_zhou = 0;
 
+        // 属性加成
+        m_gong_ji_su_du_jia_cheng = 0;
+        m_ji_chu_shang_hai_jia = 0;
+        m_ji_chu_shang_hai_jia_cheng = 0;
+        m_bao_ji_ji_lu_jia = 0;
+        m_bao_ji_jiang_li_jia = 0;
         m_shang_hai_jia_cheng = 0;
-
-        m_adjustment = 0; //目前尚不清楚，究竟差距在哪里
     }
 
-
-    DungeonHunter( size_t feng_bao, size_t ming_jie )
-        : m_feng_bao( feng_bao ),
-          m_min_jie( ming_jie )
+    void set_wu_qi( const WuQi& wu_qi )
     {
+        m_wu_qi_shang_hai_min = wu_qi.m_wu_qi_shang_hai_min;
+        m_wu_qi_shang_hai_max = wu_qi.m_wu_qi_shang_hai_max;
     }
 
-    void set_feng_bao( size_t feng_bao )
+    void set_wu_qi( double wu_qi_shang_hai_min, double wu_qi_shang_hai_max )
     {
-        m_feng_bao = feng_bao;
+        m_wu_qi_shang_hai_min = wu_qi_shang_hai_min;
+        m_wu_qi_shang_hai_max = wu_qi_shang_hai_max;
     }
 
-    void set_min_jie( size_t min_jie )
+    void set_feng_bao_fu_zhou( size_t feng_bao_fu_zhou )
     {
-        m_min_jie = min_jie;
+        m_feng_bao_fu_zhou = feng_bao_fu_zhou;
+    }
+
+    void set_min_jie_fu_zhou( size_t min_jie_fu_zhou )
+    {
+        m_min_jie_fu_zhou = min_jie_fu_zhou;
+    }
+
+    void set_dong_cha_fu_zhou( size_t dong_cha_fu_zhou )
+    {
+        m_dong_cha_fu_zhou = dong_cha_fu_zhou;
     }
 
     void set_bing_shuang_zhi_jian()
@@ -58,105 +94,109 @@ public:
 
     void set_ying_yan()
     {
-        m_bao_ji_ji_lu += 1.0;
-        m_bao_ji_jiang_li += 0.3;
-
-        double shang_hai = std::ceil( ( m_shang_hai_min + m_shang_hai_max ) / 2.0 );
-        m_adjustment = -( shang_hai * 0.1 + m_feng_bao * 0.24 * shang_hai * 0.1 );
-
         // 鹰眼技能
         // - 平均伤害的 10%
         // - 平均伤害 * 风暴符咒个数 * 风暴符咒伤害 * 10% 
+        //m_bao_ji_ji_lu += 1.0;
+        //m_bao_ji_jiang_li += 0.3;
+
+        //double shang_hai = std::ceil( ( m_wu_qi_shang_hai_min + m_wu_qi_shang_hai_max ) / 2.0 );
+        //m_adjustment = -( shang_hai * 0.1 + m_feng_bao_fu_zhou * 0.24 * shang_hai * 0.1 );
     }
 
     void set_ring( size_t bao_ji_ji_lu, size_t shang_hai )
     {
     }
 
-
-#if 0
-    double miao_shang()
+    double miao_shang_ji_suan_gong_shi()
     {
-        double m_shang_hai = std::ceil( ( m_shang_hai_min + m_shang_hai_max ) / 2.0 );
+        // 基础伤害
+        double ji_chu_shang_hai = std::ceil( ( m_wu_qi_shang_hai_min + m_wu_qi_shang_hai_max ) / 2.0 );
+        m_ji_chu_shang_hai = ji_chu_shang_hai;
+        m_ji_chu_shang_hai += m_ji_chu_shang_hai_jia; // 力量符咒
+        m_ji_chu_shang_hai += ji_chu_shang_hai * m_feng_bao_fu_zhou * 0.24; // 风暴符咒
+        m_ji_chu_shang_hai += ji_chu_shang_hai * m_ji_chu_shang_hai_jia_cheng;
 
-        double bao_ji_shang_hai = m_shang_hai * ( 1 + m_bao_ji_jiang_li ) * m_bao_ji_ji_lu;
+        // 暴击几率
+        m_bao_ji_ji_lu += m_bao_ji_ji_lu_jia;
+        m_bao_ji_ji_lu += m_dong_cha_fu_zhou * 0.1; // 洞察符咒
 
-        double pu_tong_shang_hai = m_shang_hai * ( 1 - m_bao_ji_ji_lu );
+        // 暴击奖励
+        m_bao_ji_jiang_li += m_bao_ji_jiang_li_jia;
+        m_bao_ji_jiang_li += m_min_jie_fu_zhou * 1.0; // 敏捷符咒
 
-        double miang_shang = ( bao_ji_shang_hai + pu_tong_shang_hai ) * m_gong_ji_su_du;
+        // 暴击伤害
+        double bao_ji_shang_hai = m_ji_chu_shang_hai * ( 1.0 + m_bao_ji_jiang_li ) * m_bao_ji_ji_lu;
 
-        return miang_shang;
-    }
-#endif
+        // 普通伤害
+        double pu_tong_shang_hai = 0;
 
-
-    double miao_shang_with_fu_shou()
-    {
-        double m_shang_hai = std::ceil( ( m_shang_hai_min + m_shang_hai_max ) / 2.0 );
-
-        double bao_ji_shang_hai = ( m_shang_hai * ( 1.0 + m_bao_ji_jiang_li + m_min_jie ) * m_bao_ji_ji_lu );
-
-        double pu_tong_shang_hai = ( m_shang_hai * ( 1.0 - m_bao_ji_ji_lu ) );
-
-        double shang_hai = bao_ji_shang_hai;
-        
-        if ( 0 < pu_tong_shang_hai )
+        if ( m_bao_ji_ji_lu < 1 )
         {
-            shang_hai += pu_tong_shang_hai;
+            pu_tong_shang_hai = m_ji_chu_shang_hai * ( 1.0 - m_bao_ji_ji_lu );
         }
+        
+        // 平均伤害
+        double ping_jun_shang_hai = bao_ji_shang_hai + pu_tong_shang_hai;
 
-        double miang_shang = shang_hai * m_gong_ji_su_du;
+        // 伤害
+        double shang_hai = ping_jun_shang_hai * ( 1 + m_shang_hai_jia_cheng );
 
-        return miang_shang * ( 1 + m_feng_bao * 0.24 + m_shang_hai_jia_cheng ) + m_adjustment;
+        // 秒伤
+        m_miao_shang = shang_hai * m_gong_ji_su_du;
+
+        return m_miao_shang;
     }
 
 private:
 
-    double m_shang_hai_min;
-    double m_shang_hai_max;
+    // 计算数据
+    double m_ji_chu_shang_hai;
+    double m_miao_shang;
+
+    // 属性
+    double m_wu_qi_shang_hai_min;
+    double m_wu_qi_shang_hai_max;
     double m_gong_ji_su_du;
     double m_bao_ji_ji_lu;
     double m_bao_ji_jiang_li;
 
-    // 技能
-    double m_shang_hai_jia_cheng;
-
-    double m_adjustment;
-
     // 符咒
+    size_t m_feng_bao_fu_zhou;
+    size_t m_min_jie_fu_zhou;
+    size_t m_dong_cha_fu_zhou;
 
-    size_t m_feng_bao;
-    size_t m_min_jie;
+    // 属性加成
+    double m_gong_ji_su_du_jia_cheng;
+    double m_ji_chu_shang_hai_jia;
+    double m_ji_chu_shang_hai_jia_cheng;
+    double m_bao_ji_ji_lu_jia;
+    double m_bao_ji_jiang_li_jia;
+    double m_shang_hai_jia_cheng;
 };
 
 
-#if 1
 double caculate_max_dps()
 {
     double max_dps = 0;
     size_t feng_bao = 0;
     size_t min_jie = 0;
 
-    double shang_hai_min = 72484;
-    double shang_hai_max = 85999;
+    // 圣洁
+    WuQi sheng_jie( 72484, 85999 );
 
-    double gong_ji_su_du = 0;
-    double bao_ji_ji_lu = 1.05;
-    double bo_ji_jiang_li = 0.8;
-
+    DungeonHunter jue_se;
 
     for ( size_t i = 0; i < 15; ++i )
     {
-        DungeonHunter my( shang_hai_min, shang_hai_max, gong_ji_su_du, bao_ji_ji_lu, bo_ji_jiang_li );
+        jue_se.reset();
+        jue_se.set_wu_qi( sheng_jie );
+        jue_se.set_feng_bao_fu_zhou( i );
+        jue_se.set_min_jie_fu_zhou( 15 - i );
 
-        //my.set_bing_shuang_zhi_jian();
-        //my.set_ying_yan();
-        my.set_feng_bao( i );
-        my.set_min_jie( 15 - i );
+        double dps = jue_se.miao_shang_ji_suan_gong_shi();
 
-        double dps = my.miao_shang_with_fu_shou();
-
-        if ( dps > max_dps )
+        if ( max_dps < dps )
         {
             max_dps = dps;
             feng_bao = i;
@@ -164,58 +204,34 @@ double caculate_max_dps()
         }
     }
 
-    std::cout << "Feng Bao: " << feng_bao << std::endl;
-    std::cout << "Min Jie:  " << min_jie << std::endl;
+    std::cout << "风暴符咒：" << feng_bao << std::endl;
+    std::cout << "敏捷符咒：" << min_jie << std::endl;
+    std::cout << "极限秒伤：" << std::setprecision(20) << max_dps << std::endl;
 
     return max_dps;
 }
 
-#endif
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    double shang_hai_min = 0;
-    double shang_hai_max = 0;
+    // 圣洁
+    WuQi sheng_jie( 72484, 85999 );
 
-    shang_hai_min = 72484;
-    shang_hai_max = 85999; // 17434 / 79241.5 = 0.2200123673350917
-                           //7924
-                           //9826
-                           //1902 1
-                           //3804 2 
+    // 食人魔
+    WuQi shi_ren_mo( 288, 327 );
 
+    DungeonHunter shao_bing;
 
-    //shang_hai_min = 19209;
-    //shang_hai_max = 22685;
+    shao_bing.set_wu_qi( sheng_jie );
+    shao_bing.set_feng_bao_fu_zhou( 5 );
+    shao_bing.set_min_jie_fu_zhou( 10 );
 
-    //shang_hai_min = 288;
-    //shang_hai_max = 327; //31.24 / 307.5  = 0.1015934959349593
+    double dps = shao_bing.miao_shang_ji_suan_gong_shi();
 
-    //shang_hai_min = 19209;
-    //shang_hai_max = 22685;
+    std::cout <<  std::setprecision(20) << dps << std::endl;
 
-    double gong_ji_su_du = 0;
-    double bao_ji_ji_lu = 0.05;
-    double bo_ji_jiang_li = 0.5;
-
-    DungeonHunter my( shang_hai_min, shang_hai_max, gong_ji_su_du, bao_ji_ji_lu, bo_ji_jiang_li );
-
-    my.set_min_jie( 9 );
-    my.set_feng_bao( 6 );
-    //my.set_bing_shuang_zhi_jian();
-    //my.set_ying_yan();
-
-    //double miao_shang = my.miao_shang();
-    double miao_shang_with_fu_zhou = my.miao_shang_with_fu_shou();
-
-    //std::cout.unsetf( std::ios::scientific );
-    //std::cout <<  std::setprecision(20) << miao_shang << std::endl;
-    std::cout <<  std::setprecision(20) << miao_shang_with_fu_zhou << std::endl;
-
-#if 0
-    std::cout << caculate_max_dps() << std::endl;
-#endif
+    caculate_max_dps();
 
 	return 0;
 }
