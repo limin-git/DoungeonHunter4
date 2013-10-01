be5a67be0095ce193bee97e4fb4fed370104ed8e
Limin <zhulimin.china@gmail.com>
2013-10-01 23:17:46 +0800

    daily backup

--- DungeonHunter.cpp
+++ DungeonHunter.cpp
@@ -22,6 +22,25 @@ struct WuQi
 };
 
 
+struct Ring
+{
+    Ring( double bao_ji_ji_lu_jia = 0,             // 暴击几率
+          double bao_ji_jiang_li_jia = 0,          // 暴击奖励
+          double ji_chu_shang_hai_jia = 0,         // 基础伤害
+          double yuan_su_shang_hai_jia_cheng = 0 ) // 元素伤害
+    {
+        m_bao_ji_ji_lu_jia = bao_ji_ji_lu_jia;
+        m_bao_ji_jiang_li_jia = bao_ji_jiang_li_jia;
+        m_ji_chu_shang_hai_jia = ji_chu_shang_hai_jia;
+        m_yuan_su_shang_hai_jia_cheng = yuan_su_shang_hai_jia_cheng;
+    }
+
+    double m_bao_ji_ji_lu_jia;
+    double m_bao_ji_jiang_li_jia;
+    double m_ji_chu_shang_hai_jia;
+    double m_yuan_su_shang_hai_jia_cheng;
+};
+
 
 
 class DungeonHunter
@@ -54,58 +73,77 @@ public:
         // 属性加成
         m_gong_ji_su_du_jia_cheng = 0;
         m_ji_chu_shang_hai_jia = 0;
-        m_ji_chu_shang_hai_jia_cheng = 0;
+        m_yuan_su_shang_hai_jia_cheng = 0;
         m_bao_ji_ji_lu_jia = 0;
         m_bao_ji_jiang_li_jia = 0;
-        m_shang_hai_jia_cheng = 0;
+
+        // 微调
+        m_adjustment = 0;
     }
 
+    // 武器
     void set_wu_qi( const WuQi& wu_qi )
     {
         m_wu_qi_shang_hai_min = wu_qi.m_wu_qi_shang_hai_min;
         m_wu_qi_shang_hai_max = wu_qi.m_wu_qi_shang_hai_max;
     }
 
+    // 武器
     void set_wu_qi( double wu_qi_shang_hai_min, double wu_qi_shang_hai_max )
     {
         m_wu_qi_shang_hai_min = wu_qi_shang_hai_min;
         m_wu_qi_shang_hai_max = wu_qi_shang_hai_max;
     }
 
+    // 风暴符咒
     void set_feng_bao_fu_zhou( size_t feng_bao_fu_zhou )
     {
         m_feng_bao_fu_zhou = feng_bao_fu_zhou;
     }
 
+    // 敏捷符咒
     void set_min_jie_fu_zhou( size_t min_jie_fu_zhou )
     {
         m_min_jie_fu_zhou = min_jie_fu_zhou;
     }
 
+    // 洞察符咒
     void set_dong_cha_fu_zhou( size_t dong_cha_fu_zhou )
     {
         m_dong_cha_fu_zhou = dong_cha_fu_zhou;
     }
 
+    // 被动技能：冰霜之箭
     void set_bing_shuang_zhi_jian()
     {
-        m_shang_hai_jia_cheng = 0.4;
+        m_yuan_su_shang_hai_jia_cheng += 0.4;
     }
 
+    // 被动技能：鹰眼
     void set_ying_yan()
     {
-        // 鹰眼技能
+        // 被动技能：鹰眼技能
         // - 平均伤害的 10%
         // - 平均伤害 * 风暴符咒个数 * 风暴符咒伤害 * 10% 
         //m_bao_ji_ji_lu += 1.0;
         //m_bao_ji_jiang_li += 0.3;
 
-        //double shang_hai = std::ceil( ( m_wu_qi_shang_hai_min + m_wu_qi_shang_hai_max ) / 2.0 );
-        //m_adjustment = -( shang_hai * 0.1 + m_feng_bao_fu_zhou * 0.24 * shang_hai * 0.1 );
+        m_bao_ji_ji_lu_jia += 1.0;
+        m_bao_ji_jiang_li_jia += 0.3;
+
+        double shang_hai = get_wu_qi_shang_hai();
+        m_adjustment -= shang_hai * 0.14;
+        m_adjustment -= m_feng_bao_fu_zhou * 0.24 * shang_hai * 0.1;
     }
 
-    void set_ring( size_t bao_ji_ji_lu, size_t shang_hai )
+    void set_ring( const Ring& ring )
     {
+        m_ji_chu_shang_hai_jia += ring.m_ji_chu_shang_hai_jia;
+        m_yuan_su_shang_hai_jia_cheng += ring.m_yuan_su_shang_hai_jia_cheng;
+        m_bao_ji_ji_lu_jia += ring.m_bao_ji_ji_lu_jia;
+        m_bao_ji_jiang_li_jia += ring.m_bao_ji_jiang_li_jia;
+
+        m_adjustment -= get_wu_qi_shang_hai() * 0.1 * ring.m_yuan_su_shang_hai_jia_cheng;
     }
 
     double miao_shang_ji_suan_gong_shi()
@@ -115,7 +153,7 @@ public:
         m_ji_chu_shang_hai = ji_chu_shang_hai;
         m_ji_chu_shang_hai += m_ji_chu_shang_hai_jia; // 力量符咒
         m_ji_chu_shang_hai += ji_chu_shang_hai * m_feng_bao_fu_zhou * 0.24; // 风暴符咒
-        m_ji_chu_shang_hai += ji_chu_shang_hai * m_ji_chu_shang_hai_jia_cheng;
+        m_ji_chu_shang_hai += ji_chu_shang_hai * m_yuan_su_shang_hai_jia_cheng;
 
         // 暴击几率
         m_bao_ji_ji_lu += m_bao_ji_ji_lu_jia;
@@ -140,12 +178,19 @@ public:
         double ping_jun_shang_hai = bao_ji_shang_hai + pu_tong_shang_hai;
 
         // 伤害
-        double shang_hai = ping_jun_shang_hai * ( 1 + m_shang_hai_jia_cheng );
+        double shang_hai = ping_jun_shang_hai;
 
         // 秒伤
         m_miao_shang = shang_hai * m_gong_ji_su_du;
 
-        return m_miao_shang;
+        return m_miao_shang + m_adjustment;
+    }
+
+private:
+
+    double get_wu_qi_shang_hai()
+    {
+        return std::ceil( ( m_wu_qi_shang_hai_min + m_wu_qi_shang_hai_max ) / 2.0 );
     }
 
 private:
@@ -169,10 +214,12 @@ private:
     // 属性加成
     double m_gong_ji_su_du_jia_cheng;
     double m_ji_chu_shang_hai_jia;
-    double m_ji_chu_shang_hai_jia_cheng;
+    double m_yuan_su_shang_hai_jia_cheng;
     double m_bao_ji_ji_lu_jia;
     double m_bao_ji_jiang_li_jia;
-    double m_shang_hai_jia_cheng;
+
+    // 微调
+    double m_adjustment;
 };
 
 
@@ -193,6 +240,8 @@ double caculate_max_dps()
         jue_se.set_wu_qi( sheng_jie );
         jue_se.set_feng_bao_fu_zhou( i );
         jue_se.set_min_jie_fu_zhou( 15 - i );
+        jue_se.set_bing_shuang_zhi_jian();   // 被动技能：冰霜之箭
+        jue_se.set_ying_yan();               // 被动技能：鹰眼
 
         double dps = jue_se.miao_shang_ji_suan_gong_shi();
 
@@ -221,17 +270,36 @@ int _tmain(int argc, _TCHAR* argv[])
     // 食人魔
     WuQi shi_ren_mo( 288, 327 );
 
+    // 神圣
+    WuQi shen_sheng( 19209, 22685 );
+
+    // 精神之戒
+    Ring jing_shen_zhi_jie( 0, 0.8, 0, 0.2 );
+
+    // 灰烬之戒
+    Ring hui_jin_zhi_jie( 0, 0, 0, 0.35 );
+
+
+    // 武器
+    const WuQi& wu_qi = sheng_jie;
+
+    // 戒指
+    const Ring& jie_zhi = jing_shen_zhi_jie;
+
     DungeonHunter shao_bing;
 
-    shao_bing.set_wu_qi( sheng_jie );
-    shao_bing.set_feng_bao_fu_zhou( 5 );
-    shao_bing.set_min_jie_fu_zhou( 10 );
+    shao_bing.set_wu_qi( wu_qi );           // 武器
+    shao_bing.set_ring( jie_zhi );          // 戒指
+    shao_bing.set_feng_bao_fu_zhou( 5 );    // 风暴符咒
+    shao_bing.set_min_jie_fu_zhou( 10 );    // 敏捷符咒
+    shao_bing.set_bing_shuang_zhi_jian();   // 被动技能：冰霜之箭
+    shao_bing.set_ying_yan();               // 被动技能：鹰眼
 
     double dps = shao_bing.miao_shang_ji_suan_gong_shi();
 
     std::cout <<  std::setprecision(20) << dps << std::endl;
 
-    caculate_max_dps();
+    //caculate_max_dps();
 
 	return 0;
 }
